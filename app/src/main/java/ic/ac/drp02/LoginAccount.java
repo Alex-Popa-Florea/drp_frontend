f41a043d6672af8a84e8751ea4e19fa12acdb4ea
Alex-Popa-Florea <alexpopafilip@gmail.com>
2022-06-23 11:10:28 +0100

    feat: Add login and signup screens.

---
+++ app/src/main/java/ic/ac/drp02/LoginAccount.java
@@ -0,0 +1,65 @@
+package ic.ac.drp02;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import ic.ac.drp02.databinding.LoginScreenBinding;
+
+public class LoginAccount extends Fragment {
+
+    private LoginScreenBinding binding;
+
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = LoginScreenBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText editUsername  = (EditText) binding.getRoot().findViewById(R.id.username_text_input);
+                String username = editUsername.getText().toString();
+                EditText editPassword  = (EditText) binding.getRoot().findViewById(R.id.password_text_input);
+                String password = editPassword.getText().toString();
+
+                NavHostFragment.findNavController(LoginAccount.this)
+                        .navigate(R.id.action_loginAccount_to_feedFragment);
+            }
+        });
+
+        binding.createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(LoginAccount.this)
+                        .navigate(R.id.action_loginAccount_to_signupAccount);
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
---
+++ app/src/main/java/ic/ac/drp02/SignupAccount.java
@@ -0,0 +1,60 @@
+package ic.ac.drp02;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import ic.ac.drp02.databinding.LoginScreenBinding;
+import ic.ac.drp02.databinding.SignupScreenBinding;
+
+public class SignupAccount extends Fragment {
+
+    private SignupScreenBinding binding;
+
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = SignupScreenBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText editUsername  = (EditText) binding.getRoot().findViewById(R.id.username_text_input);
+                String username = editUsername.getText().toString();
+                EditText editPassword  = (EditText) binding.getRoot().findViewById(R.id.password_text_input);
+                String password = editPassword.getText().toString();
+                EditText editConfirmPassword  = (EditText) binding.getRoot().findViewById(R.id.confirm_password_text_input);
+                String confirmPassword = editConfirmPassword.getText().toString();
+
+                NavHostFragment.findNavController(SignupAccount.this)
+                        .navigate(R.id.action_signupAccount_to_feedFragment);
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
